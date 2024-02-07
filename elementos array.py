86c215a98869ff5a409d7ab97bd24df891a472f7
Michele Rodrigues <154391130+MicheleRodriguesc@users.noreply.github.com>
2024-02-07 17:08:56 -0300

    Add files via upload

---
+++ elementos array.py
@@ -0,0 +1,21 @@
+# Importando a biblioteca para calcular o tamanho da lista
+from array import array
+
+# Copiando o array para o seu editor de código
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+
+# Imprimindo a quantidade de elementos da lista
+tamanho_lista = len(lista_musicos)
+print(f"Quantidade de elementos: {tamanho_lista}")
+
+# Imprimindo o dado salvo no índice 2
+indice_2 = lista_musicos[2]
+print(f"Dado no índice 2: {indice_2}")
+
+# Imprimindo o dado salvo no índice 9
+indice_9 = lista_musicos[9]
+print(f"Dado no índice 9: {indice_9}")
+
+# Imprimindo o dado salvo no índice 14
+indice_14 = lista_musicos[14]
+print(f"Dado no índice 14: {indice_14}")
